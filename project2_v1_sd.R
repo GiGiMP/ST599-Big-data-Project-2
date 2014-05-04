e4a5aed0f3bc1cac0cb6142decbefaf64b2a94fd
Gina S <gina.shellhammer@gmail.com>
2014-05-04 15:16:14 -0700

    First data visual
    subset united flights pdx to sfo. Add date variable for plot. summarize
    crseleapsed by day and make plot.

--- project2_v1_sd.R
+++ project2_v1_sd.R
@@ -1,4 +1,5 @@
 library(dplyr)
+
 endpoint <- "flights.cwick.co.nz"
 user <- "student"
 password <- "password"
@@ -12,26 +13,29 @@ ontime <- src_postgres("ontime",
 flights <- tbl(ontime, "flights")
 head(flights) 
 tbl_df(head(flights))
+# I am blocking out Charlotte's code as it isn't helpful to me.
+        #hou <- filter(flights, (year == "2011" & month == 1) &  
+                      (origin == "HOU" | origin == "IAH"))
+      
+        #explain(hou) 
+        #hou # now it executes
+        #hou_local <- collect(hou)
 
-hou <- filter(flights, (year == "2011" & month == 1) &  
-                (origin == "HOU" | origin == "IAH"))
-
-explain(hou) 
-hou # now it executes
-hou_local <- collect(hou)
-
-pdx <- filter(flights, origin == "PDX" & year == "2013")
-pdx_by_day <- group_by(pdx, year, month, dayofmonth)
-flights_per_day <- summarise(pdx_by_day, n_flights = n())
-explain(flights_per_day)
-
-flights_per_day # looks good
-flights_per_day$year # but it isn't behaving like a data.frame
-
-fpd <- collect(flights_per_day)
-
-fpd$date <- with(fpd, ISOdate(year, month, dayofmonth))
+#subset to get PDX to sfo flights
+pdx2sfo.ua <- filter(flights, origin == "PDX" & dest == "SFO" & uniquecarrier == "UA")
+# first I group to get the flights by day, get the mean crstime, then I will add a date variable so I can plot.
+pdx2sfo.uabyday<-group_by(pdx2sfo.ua, year, month, dayofmonth)
+pdx2sfo_ua_byday<-summarize(pdx2sfo.uabyday, mean_crselapsed=mean(crselapsedtime))
+pdx2sfo.united<-collect(pdx2sfo_ua_byday)
+pdx2sfo.united$date<-with(pdx2sfo.united, ISOdate(year, month, dayofmonth))
 
 library(ggplot2)
-qplot(date, n_flights, data = fpd, geom = "line")
+qplot(date, mean_crselapsed, data=pdx2sfo.united) + 
+  geom_smooth()
 
+#If you want to look at the data with a line geom but I think it isn't very helpful and it takes forever!
+qplot(date, mean_crselapsed, data=pdx2sfo.united, geom="line") +
+  geom_smooth()
+# there is definitely cyclic patterns. I want to  look at a 2 year spread to see whats going on.
+# I will need to look through old notes as I am having trouble remembering how to manipulate
+# dates.
